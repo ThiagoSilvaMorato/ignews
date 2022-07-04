1a02c3e87f8301871ce8b90facf913a8a8b65399
ThiagoSilvaMorato <thiago.morato@dartdigital.com.br>
2022-07-04 11:18:02 -0300

    Validando assinatura ativa

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,14 @@
 import { GetStaticProps } from "next";
+import Link from "next/link";
+import Head from "next/head";
+
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
 
-import Head from "next/head";
+import React from "react";
 
 import styles from "./styles.module.scss";
-import React from "react";
 
 type Post = {
   slug: string;
@@ -29,11 +31,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href='#'>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
