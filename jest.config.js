c1540110a3d5c394d5af05bb553d3a0e3e3b2574
Thiago <thiagosmorato@gmail.com>
2023-08-21 15:51:26 -0300

    Commit para envio de dúvida

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
-  setupFilesAfterEnv: ["<rootDir>/srv/tests/setupTests.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-  testEnvironment: "jsdom",
 };
